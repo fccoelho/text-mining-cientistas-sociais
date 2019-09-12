d9e259e8a365981f2e7939cc6e35d6a8914252e5
Flávio Codeço Coelho <fccoelho@gmail.com>
2019-09-12 12:42:21 -0300

    atualizei capitulos 2 e 3

--- dhbbmining.py
+++ dhbbmining.py
@@ -25,3 +25,40 @@ def tabula_verbete(arquivos, n=None):
         linhas.append(campos)
     tabela = pd.DataFrame(data=linhas, columns=['arquivo', 'title', 'natureza', 'sexo', 'cargos', 'corpo'])
     return tabela
+
+def parse_progenitores(linha):
+    '''
+    A partir de uma linha do DHBB contendo nomes de pai e mãe, extrai os nomes e os retorna
+    '''
+    right = linha.right[1:]
+    try:
+        pos_virg = right.index(',')
+    except ValueError:
+        pos_virg = 100
+    try:
+        pos_pt = right.index('.')
+    except ValueError:
+        pos_pt = 100
+    fim = min(pos_virg, pos_pt)
+    pais = right[:fim]
+    if 'e' in pais:
+        pai = " ".join(pais[:pais.index('e')])
+        mae = " ".join([p for p in pais[pais.index('e'):] if p not in [',', 'de', 'e']])
+    else:
+        pai = " ".join(pais)
+        mae = "desconhecida."
+    return pai, mae
+
+def encontra_pais(tc):
+    linhas = tc.concordance_list('filho',width=180, lines=50000) + tc.concordance_list('filha',width=180, lines=50000)
+    print("Processando {} linhas".format(len(linhas)))
+    i = 0
+    progenitores = []
+    for linha in linhas:
+        if linha.right[0] not in ['de', 'da', 'do']:
+            continue
+        pai, mae = parse_progenitores(linha)
+        i += 1
+        progenitores.append((pai, mae))
+    #print("Encontramos progenitores em {} linhas".format(i))
+    return progenitores
