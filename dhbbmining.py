2047f1d8e32b6c81720828564c0bf322f32ff070
Flávio Codeço Coelho <fccoelho@gmail.com>
2019-09-13 13:08:00 -0300

    adicionei modello treinado com eventos

--- dhbbmining.py
+++ dhbbmining.py
@@ -12,7 +12,7 @@ def tabula_verbete(arquivos, n=None):
         n = len(arquivos)
     linhas = []
     for a in arquivos[:n]:
-        with open(a, 'r') as f:
+        with open(a, 'r', encoding='utf8') as f:
             verbete = f.read()
         cabeçalho = verbete.split('---')[1]
         campos = {l.split(':')[0].strip(): l.split(':')[1].strip() for l in cabeçalho.split('\n')[:4] if l}
